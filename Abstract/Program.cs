ec7c71ac2acc0e8907de2885fbd5efe3b46c5ea9
DEEPAK SHUKLA <DEEPAK SHUKLA@DEEPAKSHUKLA>
2016-06-27 13:11:25 +0530

    Add project files.

---
+++ Abstract/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+
+    abstract class user
+    {
+
+    }
+}
